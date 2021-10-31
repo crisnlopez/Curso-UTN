a924b0042a31635ffef87133226fdc91417cb8ea
Cristian Lopez <79811937+crisnlopez@users.noreply.github.com>
2021-10-31 19:20:03 -0300

    agregar y modificar

--- Modulo-6/CRUD/routes/admin/novedades.js
+++ Modulo-6/CRUD/routes/admin/novedades.js
@@ -20,5 +20,66 @@ router.get('/eliminar/:id', async function (req, res, next) {
   res.redirect('/admin/novedades');
 });
 
+// Agregar novedad
+router.get('/agregar', (req, res, next) => {
+  res.render('admin/agregar', {
+    layout: 'admin/layout'
+  })
+});
+
+router.post('/agregar', async (req, res , next) => {
+  try {
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.addNovedades(req.body);
+      res.redirect('/admin/novedades')
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout', 
+        error: true, 
+        message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    res.render('admin/agregar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se cargo la novedad'
+    })
+  }
+})
+
+// Modificar novedad
+// traer novedad
+router.get('/modificar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  var novedad = await novedadesModel.getNovedadById(id);
+
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+})
+
+// modificar la novedad
+router.post('/modificar', async (req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  } catch (error) {
+    console.error(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true, 
+      message: 'No se modifico la novedad'
+    })
+  }
+})
+
 
 module.exports = router;
